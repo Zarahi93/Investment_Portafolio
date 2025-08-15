ac5b8dcaee1d08e60e6f8baa8a6ee70c98792353
Zarahi93 <adahi.g.z@hotmail.com>
2025-08-15 16:19:22 -0600

    Creating a server to run the page and creating the routes for the navigation on the pages

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-// server.js
-
 const express = require('express');
 const path = require('path');
+
 const app = express();
-const port = 3000;
 
-// Serve static files (CSS, JS, Images) from the 'assets' and 'css' directories
-app.use(express.static(path.join(__dirname, 'images')));
-app.use(express.static(path.join(__dirname, 'main')));
-app.use(express.static(path.join(__dirname, 'scripts')));
-
-// Serve the SPA's HTML file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+// Route to serve index.html from same directory
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+// Routes /logIn - logIn.html (inside /html)
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html', 'logIn.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html', 'signUp.html'));
 });
+
+app.get('/dashboard', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html', 'dashboard.html'));
+});
+
+
+// Port Number
+const PORT = process.env.PORT || 5000;
+
+// Start the server
+app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
