69efea8c58df7b0aa948e5a339e96f95cd82c934
Zarahi93 <adahi.g.z@hotmail.com>
2025-08-18 00:21:21 -0600

    Modifying the server file so loging became the home page and create the startServer function to handle the starting of the server

--- server.js
+++ server.js
@@ -7,15 +7,23 @@ const serveHtml = (filePath) => (req, res) => {
     res.sendFile(path.join(__dirname, ...filePath));
 };
 
-app.get('/', serveHtml(['index.html']));
-app.get('/login', serveHtml(['html', 'logIn.html']));
+app.get('/', serveHtml(['html', 'logIn.html']));
+app.get('/recover', serveHtml(['html', 'recoverPassword.html']));
 app.get('/signup', serveHtml(['html', 'signUp.html']));
 app.get('/quiz', serveHtml(['html', 'quiz.html']));
 app.get('/dashboard', serveHtml(['html', 'dashboard.html']));
 
-if (require.main === module) {
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
+function startServer() {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () =>
+    console.log(`Server started on http://localhost:${PORT}`)
+  );
 }
 
-module.exports = app;
+/* istanbul ignore next */
+if (require.main === module) {
+  startServer();
+}
+
+
+module.exports = {app, startServer};
