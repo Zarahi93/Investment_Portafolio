820d78cb6adeddc0fb7f747e476c9ac424c050c6
Zarahi93 <adahi.g.z@hotmail.com>
2025-08-17 22:07:36 -0600

    Leabing the files clear the navegation is handled by the server file

--- scripts/pages/dashboard.js
+++
@@ -1,8 +0,0 @@
-// Managing the buttons for changing pages begins ---->
-const logOut = document.getElementById("logOut");
-logOut.addEventListener("click", goToLogIn);
-
-function goToLogIn(){ 
-    window.location.assign("./logIn.html")
-}
-// <---- Managing the buttons for changing pages ends
